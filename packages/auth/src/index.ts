b34944fe908fc7f1882fa5d536321bdae1800551
Menachem Hornbacher <mhornbacher@kiddom.co>
2025-12-19 10:57:14 -0500

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { expo } from '@better-auth/expo';
+import { db } from "@sentry-reproduction/db";
+import * as schema from "@sentry-reproduction/db/schema/auth";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || "", "mybettertapp://", "exp://"],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [expo()]
+});
